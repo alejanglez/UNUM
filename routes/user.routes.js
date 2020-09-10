3d18725924160c5f35b2671854ab6e15e224cfae
Nuno Pacheco <nunorpacheco@gmail.com>
2020-09-10 14:02:31 +0100

    Changes on headers style

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,17 +9,23 @@ const Job = require("../models/Job.model");
 const mongoose = require("mongoose");
 
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 router.get("/profile-user", (req, res, next) => {
-  res.render("profileuser", {userInSession: req.session.currentUser});
+  res.render("profileuser", {
+    userInSession: req.session.currentUser
+  });
   console.log(req.body)
 })
 //EDIT USER PROFILE
 router.get('/edit', (req, res) => {
- 
 
-  res.render('edit-details.hbs', {userInSession:req.session.currentUser})
+
+  res.render('edit-details.hbs', {
+    userInSession: req.session.currentUser
+  })
 })
 
 router.post("/edit", (req, res) => {
@@ -34,22 +40,25 @@ router.post("/edit", (req, res) => {
     skill1,
     skill2,
     skill3,
-  
   } = req.body;
   console.log("Profile edit", req.body)
 
-  User.findByIdAndUpdate( userData._id, req.body, {new: true})
-  .then((currentUser) =>  {
-    req.session.currentUser = currentUser
-    res.redirect('profile-user')
-  })
+  User.findByIdAndUpdate(userData._id, req.body, {
+      new: true
+    })
+    .then((currentUser) => {
+      req.session.currentUser = currentUser
+      res.redirect('profile-user')
+    })
 })
 
 //DELETE USER PROFILE
-router.get('/delete', (req, res) => {
-  const {id} = req.params;
+router.post('/user/:id/delete', (req, res) => {
+  const {
+    id
+  } = req.params;
   User.findByIdAndDelete(id)
-    .then(() => res.redirect('deletedprofile')) //doing logout when delete it
+    .then(() => res.redirect('/index')) //doing logout when delete it
     .catch((err) => {
       console.log(`Error while deleting the profile: ${err}`);
       next();
